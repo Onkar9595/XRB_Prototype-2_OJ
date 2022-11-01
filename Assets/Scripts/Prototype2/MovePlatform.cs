9b4e9e6156e1b47f27a9a834ce638a353d625d7c
Onkar9595 <111443758+Onkar9595@users.noreply.github.com>
2022-11-01 15:54:39 -0400

    wip

---
+++ Assets/Scripts/Prototype2/MovePlatform.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovePlatform : MonoBehaviour
+{
+    [SerializeField] private Transform _target = default;
+
+    private Vector3 _initialPosition; 
+
+    [SerializeField] private float _duration = 2;
+
+    [SerializeField] private Ease _ease = Ease.Linear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _initialPosition = transform.position;
+    }
+    public void MoveToTarget()
+    {
+        Move(_target.position);
+    }
+
+    public void MoveBack()
+    {
+        Move(_initialPosition);
+    }
+
+    private void Move(Vector3 position)
+    {
+        transform.DOMove(position, _duration)
+            .SetEase(_ease)
+            .OnComplete(() => Debug.Log("Position Reached"));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triggered");
+            MoveToTarget();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // MoveToTarget();
+    }
+}
